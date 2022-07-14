6b0292ca1f137201afce07bb577d9e2aa31bb70d
Daiki Komagata <s1290196@u-aizu.ac.jp>
2022-07-14 12:10:58 +0900

    HoT: initial revision

---
+++ HoT.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+    int a,k=0,l=0;
+    printf("Tossing a coin...\n");
+    for(int i = 1; i <= 3; i++) {
+        a=rand()%2;
+        if(a){
+            printf("Round %d:Heads\n",i);
+            k++;
+        }
+        else{
+            printf("Round %d:Tails\n",i);
+            l++;
+        }
+    }
+    printf("Heads; %d, Tails: %d\n",k,l);
+    return 0;
+}
